059b84cc56d6355d21602d1a1a523545c9d0f0e9
Rogger Vasquez <rvasque3@gmail.com>
2021-10-06 19:34:32 -0500

    feature(mathUtils): Display which number is bigger between two inputs

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function showNumber(number) {
     console.log(`El numero es: ${number}`)
     console.log('No se???')
 }
+
+const whoIsBigger = (a, b) => {
+    return a > b ? `El mayor es ${a}`:` el mayor es ${b}`
+}
